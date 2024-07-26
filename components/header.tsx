427b5f573781ec815241eff2bebe19cd5915e291
Anmol Baranwal <anmolbaranwal119@gmail.com>
2024-07-26 14:48:54 +0530

    chore: add header component for inner pages

---
+++ components/header.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+
+interface HeaderProps {
+  title: string
+  docsLink: string
+}
+
+const Header: React.FC<HeaderProps> = ({ title, docsLink }) => {
+  return (
+    <div className="px-6 pt-20">
+      <h2 className="text-3xl font-extrabold leading-tight tracking-tighter text-white md:text-4xl">
+        {title}
+      </h2>
+      <p className="pt-2 text-lg text-gray-300">
+        Read more on the{' '}
+        <Link href={docsLink} className="border-b border-gray-300">
+          docs
+        </Link>
+        .
+      </p>
+      <div className="w-full border-b-2 border-gray-700 pt-8" />
+    </div>
+  )
+}
+
+export default Header
